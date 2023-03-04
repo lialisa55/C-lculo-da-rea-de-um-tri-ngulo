b7894247bcfb7ae0bbed7277b271bd8ffb793d4c
unknown <nathalia.mota@etec.sp.gov.br>
2023-03-03 21:02:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Área_de_um_Triângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira as medidas dos três lados");
+            string l1 = Console.ReadLine();
+            string l2 = Console.ReadLine();
+            string l3 = Console.ReadLine();
+            Convert.ToInt32(l1);
+            Convert.ToInt32(l2);
+            Convert.ToInt32(l3);
+
+            
+
+
+            int p = (l1 + l2 + l3) / 2;
+
+
+        }
+    }
+}
